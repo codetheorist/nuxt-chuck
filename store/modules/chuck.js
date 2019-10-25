6ffe1dba7c7d02817ac2cfd4502d4c4ee730cded
Alex Scott <alex@codetheoristdevelopments.co.uk>
2019-10-25 10:14:08 +0100

    Functional store

--- store/modules/chuck.js
+++ store/modules/chuck.js
@@ -1,13 +1,20 @@
+const fs = require('fs')
+let db = []
+fs.readFile('../../jokes.json', 'utf-8', (err, data) => {
+  if(err) { throw err; }
+  db = JSON.parse(data)
+})
 // store/modules/counter.js
 export default options => ({
   namespaced: true,
   state: () => ({
     options,
+    jokes: db,
     joke: {}
   }),
   mutations: {
     random(state) {
-      state.joke = ''
+      state.joke = state.db[Math.floor(Math.random() * state.db.length)]
     }
   },
   getters: {
